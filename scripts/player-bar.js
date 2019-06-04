2c81308533b1cdb20112f7c03b54a9abb32b563d
Cam <camkliao@gmail.com>
2019-06-03 20:39:33 -0400

    added previous button function

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -10,8 +10,25 @@
      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
      const nextSongIndex = currentSongIndex + 1;
      if (nextSongIndex >= album.songs.length) { return; }
-     
+
      const nextSong = album.songs[nextSongIndex];
      player.playPause(nextSong);
    });
+
+   $('#time-control input').on('input', function (event) {
+   player.skipTo(event.target.value);
+ });
 }
+
+//Previous button function
+
+$('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex < 0 ) { return; }
+    
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+})
